42baa4b8f45fa5f5dfc586695d5ce34bdcefd2a1
abhishek <abhiskg20@gmail.com>
2022-12-14 20:49:59 +0600

    Header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Header from "./components/Header";
+
 function App() {
-  return <div>Welcome CoderVerse</div>;
+  return (
+    <div>
+      <h1>Welcome CoderVerse</h1>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
